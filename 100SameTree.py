99c9f7d44a8f64ba11256840fe16cc0d3a98abd5
wuyuandi <yuandi@ualberta.ca>
2019-08-31 14:32:05 -0600

    第一周总结

---
+++ 100SameTree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        elif p == None or q == None:
+            return False
+        elif p.val == q.val:
+            return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        else:
+            return False
+        
+#先判断both非空，再判断值相同 递归func找left 和 right 值是否相同
+#
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # leetcode_Prob
-Language: python3
-some code are found in internet and some are mine
+##Language: python3  
+###some code are found in internet and some are mine  
++ 第一周复习  range()的用法 List的切片 【：：-1】 enumerate的使用
+
+
